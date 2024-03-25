ebc89e40b0c7bed5fbb1f3239338740907cb5868
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2024-03-25 10:41:17 +0300

    added dropdown menu to searchbar

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,12 @@
+import "./dropdown.style.scss"
+const Dropdown = () => {
+    return (
+        <div className="dropdown">
+            <p>Ülke 1</p>
+            <p>Ülke 2</p>
+            <p>Ülke 3</p>
+        </div>
+    )
+}
+
+export default Dropdown
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,46 +1,77 @@
 import { CiSearch } from "react-icons/ci";
 import "./search.style.scss";
+import { IoIosArrowDown } from "react-icons/io";
+import Dropdown from "../Dropdown/Dropdown";
+import { useState } from "react";
+
 const Search = () => {
+    const [showLocationDropdown, setShowLocationDropdown] = useState(false);
+    const [showReligionDropdown, setShowReligionDropdown] = useState(false);
+    const [showYearDropdown, setShowYearDropdown] = useState(false);
+    const [showTypeDropdown, setShowTypeDropdown] = useState(false);
+
+    const toggleLocationDropdown = () => {
+        setShowLocationDropdown(!showLocationDropdown);
+        setShowReligionDropdown(false);
+        setShowYearDropdown(false);
+        setShowTypeDropdown(false);
+    };
+
+    const toggleReligionDropdown = () => {
+        setShowReligionDropdown(!showReligionDropdown);
+        setShowLocationDropdown(false);
+        setShowYearDropdown(false);
+        setShowTypeDropdown(false);
+    };
+
+    const toggleYearDropdown = () => {
+        setShowYearDropdown(!showYearDropdown);
+        setShowLocationDropdown(false);
+        setShowReligionDropdown(false);
+        setShowTypeDropdown(false);
+    };
+
+    const toggleTypeDropdown = () => {
+        setShowTypeDropdown(!showTypeDropdown);
+        setShowLocationDropdown(false);
+        setShowReligionDropdown(false);
+        setShowYearDropdown(false);
+    };
+
     return (
         <div className="search-container">
             <form className="search-context">
-                <div className="search-category">
+                <div className="search-category" onClick={toggleLocationDropdown}>
                     <h1>Location</h1>
-                    <select required>
-                        <option selected disabled value="all">
-                            Country
-                        </option>
-                        <option value="gun">New Zeeland</option>
-                        <option value="knife">USA</option>
-                        <option value="sword">Japan</option>
-                    </select>
+                    <div className="dropdown-container">
+                        <p>Norway</p>
+                        <IoIosArrowDown />
+                    </div>
+                    {showLocationDropdown && <Dropdown />}
                 </div>
-                <div className="search-category">
+                <div className="search-category" onClick={toggleReligionDropdown}>
                     <h1>Religion</h1>
-                    <select required>
-                        <option selected>Asatro</option>
-                        <option>Gun</option>
-                        <option>Knife</option>
-                        <option>Sword</option>
-                    </select>
+                    <div className="dropdown-container">
+                        <p>Asatro</p>
+                        <IoIosArrowDown />
+                    </div>
+                    {showReligionDropdown && <Dropdown />}
                 </div>
-                <div className="search-category">
+                <div className="search-category" onClick={toggleYearDropdown}>
                     <h1>Year</h1>
-                    <select required>
-                        <option selected>793 AD</option>
-                        <option>2000 AD</option>
-                        <option>1230 AD</option>
-                        <option>1432 AD</option>
-                    </select>
+                    <div className="dropdown-container">
+                        <p>793 AD</p>
+                        <IoIosArrowDown />
+                    </div>
+                    {showYearDropdown && <Dropdown />}
                 </div>
-                <div className="search-category">
+                <div className="search-category" onClick={toggleTypeDropdown}>
                     <h1>Type</h1>
-                    <select required>
-                        <option value="all">Weapons</option>
-                        <option value="gun">Gun</option>
-                        <option value="knife">Knife</option>
-                        <option value="sword">Sword</option>
-                    </select>
+                    <div className="dropdown-container">
+                        <p>Weapons</p>
+                        <IoIosArrowDown />
+                    </div>
+                    {showTypeDropdown && <Dropdown />}
                 </div>
                 <button type="submit">
                     <CiSearch />
